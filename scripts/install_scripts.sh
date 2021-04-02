d3d7dc2c05c47d707c684a5dd2a51151a59feb48
tayo <tayo-p@live.co.uk>
2021-04-02 18:50:41 +0000

    added k8s and eks install scripts

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -8,6 +8,7 @@ sudo apt upgrade -y
 sudo apt install curl -y
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker jenkins
 
 #install Ansible
 sudo apt install software-properties-common
@@ -20,12 +21,3 @@ wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd6
 unzip terraform_*_linux_*.zip
 sudo mv terraform /usr/local/bin
 rm terraform_*_linux_*.zip
-
-#install Kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-
-#install eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"| tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
