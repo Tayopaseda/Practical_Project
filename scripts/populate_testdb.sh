c2c998dedc2efeede85612fa9a701b5a73e0f11f
tayo <tayo-p@live.co.uk>
2021-04-04 20:42:34 +0000

    edited ssh command to stop hostchecking

--- scripts/populate_testdb.sh
+++ scripts/populate_testdb.sh
@@ -2,18 +2,16 @@
 
 source ~/.bash_profile
 
-ssh -i ~/.ssh/id_rsa ubuntu@$testIP << EOF
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$testIP << EOF
 sudo apt update -y
 sudo apt install mysql-client-core-8.0 -y 
 EOF
 
-ssh -i ~/.ssh/id_rsa ubuntu@$testIP mysql -h {{endpoint-address}} -P 3306 -u {{user}} -p{{password}} << EOF
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$testIP mysql -h {{endpoint-address}} -P 3306 -u {{user}} -p{{password}} << EOF
 CREATE DATABASE users;
 USE users;
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `userName` varchar(30) NOT NULL
-);
+CREATE TABLE `users` (`userName` varchar(30) NOT NULL);
 INSERT INTO `users` VALUES ('Bob'),('Jay'),('Matt'),('Ferg'),('Mo');
 EOF
 
