061677187e2caa5fe824b845f182706b90f7cdcc
tayo <tayo-p@live.co.uk>
2021-04-04 13:10:32 +0000

    fixed issues with terraform

--- Terraform/subnets/variables.tf
+++ Terraform/subnets/variables.tf
@@ -3,14 +3,14 @@ variable "vpc_id" {
 }
 
 variable "rds-1-cidr" {
-  default = "28.2.0.0/24"
+  default = "28.7.0.0/24"
 }
 
 variable "rds-2-cidr" {
-  default = "28.2.1.0/24"
+  default = "28.7.1.0/24"
 }
 
 variable "test-vm-cidr" {
-  default = "28.2.2.0/24"
+  default = "28.7.2.0/24"
 }
 
