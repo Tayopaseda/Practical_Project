44224b994a725dd0c20148d8a507bfc31e80f027
tayo <tayo-p@live.co.uk>
2021-04-04 13:44:48 +0000

    added multiple availability zones for subnets

--- Terraform/subnets/main.tf
+++ Terraform/subnets/main.tf
@@ -5,10 +5,12 @@ resource "aws_subnet" "test-vm" {
 
 resource "aws_subnet" "rds-1" {
   vpc_id = var.vpc_id
+  availability_zone = "eu-west-2a"
   cidr_block = var.rds-1-cidr
 }
 
 resource "aws_subnet" "rds-2" {
   vpc_id = var.vpc_id
+  availability_zone = "eu-west-2b"
   cidr_block = var.rds-2-cidr
 }
