7a58a789d7ed4ca099eb0532b6b1f617ffdef802
tayo <tayo-p@live.co.uk>
2021-04-05 16:38:16 +0000

    frontend test fix

--- scripts/populate_testdb.sh
+++ scripts/populate_testdb.sh
@@ -8,8 +8,8 @@ sudo apt install mysql-client-core-8.0 -y
 EOF
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$testIP mysql -h {{endpoint-address}} -P 3306 -u {{user}} -p{{password}} << EOF
-CREATE DATABASE users;
-USE users;
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (userName varchar(30) NOT NULL);
 INSERT INTO users VALUES ('Bob'),('Jay'),('Matt'),('Ferg'),('Mo');
