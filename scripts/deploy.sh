f39b0a29ff9ccecbb26c0c1b8863f1da63345dd1
tayo <tayo-p@live.co.uk>
2021-04-06 17:53:27 +0000

    added deploy script and stage

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd ./kubernetes
+
+kubectl apply -f namespace.yaml
+kubectl config set-context --current --namespace=production
+kubectl apply -f nginx-config.yaml 
+kubectl apply -f nginx-lb.yaml -f nginx.yaml -f backend.yaml -f backend-dep.yaml -f frontend.yaml -f frontend-dep.yaml 
+
+kubectl rollout restart deployment backend
+kubectl rollout restart deployment flask-app
+
+kubectl get services nginx-lb
